48348ef5e90e483b9cc32c34e71432513f3a7a19
Tanmay Tupe <tanmaytupe@gmail.com>
2022-04-22 20:16:17 +0530

    board class can now give result depending upon positions of symbols

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -14,12 +14,39 @@ module TicTacToe
         LOGGER.info("position #{position.inspect} is already occupied by symbol #{symbol_at_position}")
         raise InvalidPositionError.new(position, self)
       else
-        @grid[position.x][position.y] = symbol
+        @grid[position.y][position.x] = symbol
       end
     end
 
     def symbol_at(position)
-      @grid[position.x][position.y]
+      @grid[position.y][position.x]
+    end
+
+    def result
+      [
+        TicTacToe::CIRCLE,
+        TicTacToe::CROSS
+      ].each do |symbol|
+        # checking for horizontal victory
+        @grid.each do |row|
+          return { verdict: 'win', victor: symbol } if row.all? { |s| s == symbol }
+        end
+
+        # checking for veritical victory
+        @grid.transpose.each do |column|
+          return { verdict: 'win', victor: symbol } if column.all? { |s| s == symbol }
+        end
+      end
+
+      # Checking for diagonal victory
+      symbol_at_center = symbol_at(Position.new(x: 1, y: 1))
+      if symbol_at_center && diagonal_victory_for?(symbol_at_center)
+        return { verdict: 'win', victor: symbol_at_center }
+      end
+
+      return { verdict: 'draw' } if all_positions_filled?
+
+      nil
     end
 
     class InvalidPositionError < StandardError
@@ -28,6 +55,43 @@ module TicTacToe
         @position = position
         @board = board
       end
+    end
+
+    private
+
+    def all_positions_filled?
+      !(@grid.flatten.any? { |s| s == nil })
+    end
+
+    def diagonal_victory_for?(symbol)
+      diagonals.each do |diagonal|
+        return true if diagonal.all? { |s| s == symbol }
+      end
+
+      false
+    end
+
+    def diagonals
+      [
+        symbols_at_diagonal_with_0_0,
+        symbols_at_diagonal_with_0_2
+      ]
+    end
+
+    def symbols_at_diagonal_with_0_0
+      [
+        symbol_at(Position.new(x: 0, y: 0)),
+        symbol_at(Position.new(x: 1, y: 1)),
+        symbol_at(Position.new(x: 2, y: 2))
+      ]
+    end
+
+    def symbols_at_diagonal_with_0_2
+      [
+        symbol_at(Position.new(x: 0, y: 2)),
+        symbol_at(Position.new(x: 1, y: 1)),
+        symbol_at(Position.new(x: 2, y: 0))
+      ]
     end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,5 +31,111 @@ module TicTacToe
         end
       end
     end
+
+    describe '#result' do
+      let(:board) { Board.new() }
+      context 'Board position does not conclude the game' do
+        subject { board.result }
+        it "returns nil" do
+          expect(subject).to be_nil
+        end
+      end
+
+      context 'Board position corresponds to a player winning horizontally' do
+        let(:winning_symbol) { TicTacToe::CIRCLE }
+        before do
+          # We are creating following scenario
+          # | - | - | - |
+          # | - | - | - |
+          # | O | O | O |
+          board.place(winning_symbol, Position.new(x: 0, y: 2))
+          board.place(winning_symbol, Position.new(x: 1, y: 2))
+          board.place(winning_symbol, Position.new(x: 2, y: 2))
+        end
+
+        subject { board.result }
+        it 'returns victory for O' do
+          expect(subject).to eq({verdict: 'win', victor: winning_symbol})
+        end
+      end
+
+      context 'Board position corresponds to a player winning virtically' do
+        let(:winning_symbol) { TicTacToe::CROSS }
+
+        before do
+          # We are creating following scenario
+          # | X | - | - |
+          # | X | - | - |
+          # | X | - | - |
+          board.place(winning_symbol, Position.new(x: 0, y: 0))
+          board.place(winning_symbol, Position.new(x: 0, y: 1))
+          board.place(winning_symbol, Position.new(x: 0, y: 2))
+        end
+
+        subject { board.result }
+        it 'returns victory for X' do
+          expect(subject).to eq({verdict: 'win', victor: winning_symbol})
+        end
+      end
+
+      context 'Board position corresponds to a player winning diagonally on diagonal from 0,0' do
+        let(:winning_symbol) { TicTacToe::CROSS }
+        before do
+          # We are creating following scenario
+          # | X | - | - |
+          # | - | X | - |
+          # | - | - | X |
+          board.place(winning_symbol, Position.new(x: 0, y: 0))
+          board.place(winning_symbol, Position.new(x: 1, y: 1))
+          board.place(winning_symbol, Position.new(x: 2, y: 2))
+        end
+
+        subject { board.result }
+        it 'returns victory for X' do
+          expect(subject).to eq({verdict: 'win', victor: winning_symbol})
+        end
+      end
+
+      context 'Board position corresponds to a player winning diagonally on diagonal from 0,2' do
+        let(:winning_symbol) { TicTacToe::CIRCLE }
+        before do
+          # We are creating following scenario
+          # | - | - | O |
+          # | - | O | - |
+          # | O | - | - |
+          board.place(winning_symbol, Position.new(x: 0, y: 0))
+          board.place(winning_symbol, Position.new(x: 1, y: 1))
+          board.place(winning_symbol, Position.new(x: 2, y: 2))
+        end
+
+        subject { board.result }
+        it 'returns victory for O' do
+          expect(subject).to eq({verdict: 'win', victor: winning_symbol})
+        end
+      end
+
+      context 'Board position corresponds to draw' do
+        before do
+          # We are creating following scenario
+          # | X | O | X |
+          # | X | O | X |
+          # | O | X | O |
+          board.place(TicTacToe::CROSS, Position.new(x: 0, y: 0))
+          board.place(TicTacToe::CIRCLE, Position.new(x: 1, y: 0))
+          board.place(TicTacToe::CROSS, Position.new(x: 2, y: 0))
+          board.place(TicTacToe::CROSS, Position.new(x: 0, y: 1))
+          board.place(TicTacToe::CIRCLE, Position.new(x: 1, y: 1))
+          board.place(TicTacToe::CROSS, Position.new(x: 2, y: 1))
+          board.place(TicTacToe::CIRCLE, Position.new(x: 0, y: 2))
+          board.place(TicTacToe::CROSS, Position.new(x: 1, y: 2))
+          board.place(TicTacToe::CIRCLE, Position.new(x: 2, y: 2))
+        end
+
+        subject { board.result }
+        it 'returns draw' do
+          expect(subject).to eq({verdict: 'draw'})
+        end
+      end
+    end
   end
 end
