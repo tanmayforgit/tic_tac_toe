db59178755c2309b033488e41cdd93f763131ef3
Tanmay Tupe <tanmaytupe@gmail.com>
2022-04-22 17:39:08 +0530

    position initialization now raises exception if position is out of standard tic tac toe board

--- lib/tic_tac_toe/position.rb
+++ lib/tic_tac_toe/position.rb
@@ -7,6 +7,10 @@ module TicTacToe
     # will raise InvalidPositionError if position is outside of standard tic tac toe
     # board size of 3 i.e. x and y should always be between 0 and 2
     def initialize(x:, y:)
+      x = x.to_i
+      y = y.to_i
+      raise InvalidPositionError.new unless x.between?(0,2)
+      raise InvalidPositionError.new unless y.between?(0,2)
       @x = x.to_i
       @y = y.to_i
     end
@@ -14,5 +18,7 @@ module TicTacToe
     def inspect
       "(#{@x}, #{@y})"
     end
+
+    class InvalidPositionError < StandardError; end
   end
 end
--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -1,11 +1,20 @@
 module TicTacToe
   RSpec.describe Position do
     describe 'Initialization' do
-      subject { Position.new(x: 2, y: 0) }
-      it "returns position object" do
-        expect(subject).to be_an_instance_of(Position)
-        expect(subject.x). to eq(2)
-        expect(subject.y). to eq(0)
+      context "When valid args are passed" do
+        subject { Position.new(x: 2, y: 0) }
+        it "returns position object" do
+          expect(subject).to be_an_instance_of(Position)
+          expect(subject.x). to eq(2)
+          expect(subject.y). to eq(0)
+        end
+      end
+
+      context "When args correspond to position out of board or negative co ordinates" do
+        it "Raises InvalidPositionError" do
+          expect { Position.new(x: 3, y: 0)}.to raise_error(Position::InvalidPositionError)
+          expect { Position.new(x: -1, y: 0)}.to raise_error(Position::InvalidPositionError)
+        end
       end
     end
   end
