e5e3ea1510d6b8b1e731b179da02a79c09699ede
Tanmay Tupe <tanmaytupe@gmail.com>
2022-04-24 16:00:32 +0530

    refactored command line interface to be a dumb stateless module

--- lib/tic_tac_toe/command_line_interface.rb
+++ lib/tic_tac_toe/command_line_interface.rb
@@ -1,123 +1,83 @@
 module TicTacToe
-  class CommandLineInterface
-    def initialize(game = Game.new)
-      @game = game
-    end
-
-    def start
-      action_to_perform = @game.action_to_perform
-
-      while !game_ending_action?(action_to_perform)
-        print_action_errors_if_any(action_to_perform)
-
-        case action_to_perform.name
-        when :give_introduction
-          puts_introduction
-          @game.start
-        when :get_p1_name
-          puts "Please enter p1 name"
-          name = STDIN.gets().strip
-          @game.accept_p1_name(name)
-        when :get_p2_name
-          puts "Please enter p2 name"
-          name = STDIN.gets().strip
-          @game.accept_p2_name(name)
-        when :get_p1_move
-          print_game_board
-          puts "Please enter p1 move"
-          position = get_position_from_command_line
-          @game.accept_p1_move(position)
-        when :get_p2_move
-          print_game_board
-          puts "Please enter p2 move"
-          position = get_position_from_command_line
-          @game.accept_p2_move(position)
-        end
-
-        action_to_perform = @game.action_to_perform
+  module CommandLineInterface
+   class << self
+      def print_board(board)
+        puts board
       end
 
-      execute_game_end(action_to_perform)
-    end
-
-    private
-
-    def print_game_board
-      puts @game.board
-    end
-
-    def get_position_from_command_line
-      user_position_input = STDIN.gets().strip
-
-      Position.from_string(user_position_input)
-    rescue Position::IncorrectFormat => e
-      print_error('Please try again with correct format')
-      get_position_from_command_line
-    rescue Position::PositionOutOfBoard => e
-      print_error('Please enter co-ordinates within the board')
-      get_position_from_command_line
-    end
-
-    def print_action_errors_if_any(action_to_perform)
-      errors = action_to_perform.errors
-
-      if errors.any?
-        error_message = errors.join(', ')
-        print_error(error_message)
-      end
-    end
-
-    def print_error(emsg)
-      puts "\e[31m#{emsg}\e[0m"
-    end
-
-    def game_ending_action?(action)
-      action.name == :announce_victory || action.name == :announce_draw
-    end
-
-    def execute_game_end(game_ending_action)
-      if game_ending_action.name == :announce_draw
-        puts_within_dashed_lines("Game was a draw")
+      def print_error(emsg)
+        puts "\e[31m#{emsg}\e[0m"
       end
 
-      if game_ending_action.name == :announce_victory
-        victor = game_ending_action.details.fetch(:name)
-        puts_within_dashed_lines("#{victor} won the game")
+      def print_introduction
+        puts_within_dashed_lines("Welcome to Command line Tic Tac Toe")
+
+        introductory_board = Board.new()
+        introductory_circle_position = Position.new(x: 1, y: 2)
+        introductory_cross_position = Position.new(x: 2, y: 0)
+        introductory_board.place(TicTacToe::CIRCLE, introductory_circle_position)
+        introductory_board.place(TicTacToe::CROSS, introductory_cross_position)
+
+        puts "Instructions:"
+        puts "1. Player one always has X and player two always has O."
+        puts "2. Game starts with accepting the player names."
+        puts "3. Game then alteratively allows players to make their moves."
+        puts "4. x axis increments towrds right and y axis increments towards downward direction starting from 0"
+        puts "5. To place X like shown below a player will have to "\
+            "enter 2,0 when his turn comes.\n"\
+            "   To place O like shown below a player will have to"\
+            "enter 1,2 when his turn comes.\n\n"
+        puts introductory_board
+
       end
 
-      print_game_board
-    end
+      def get_name(name_holder)
+        puts("Please enter #{name_holder} name")
+        STDIN.gets.strip()
+      end
 
-    def puts_within_dashed_lines(string)
-      puts(dashed_line)
-      puts("\n#{string}\n")
-      puts(dashed_line)
-    end
+      def get_move(board, mover_name)
+        puts board
+        puts("#{mover_name}, please enter your move")
+        get_position_from_command_line
+      end
 
-    def puts_introduction
-      puts_within_dashed_lines("Welcome to Command line Tic Tac Toe")
+      def announce_draw(board)
+        puts_within_dashed_lines('Game was a draw')
+        puts board
+      end
 
-      introductory_board = Board.new()
-      introductory_circle_position = Position.new(x: 1, y: 2)
-      introductory_cross_position = Position.new(x: 2, y: 0)
-      introductory_board.place(TicTacToe::CIRCLE, introductory_circle_position)
-      introductory_board.place(TicTacToe::CROSS, introductory_cross_position)
+      def announce_victory(board, victor_name)
+        puts_within_dashed_lines("#{victor_name} won the game")
+        puts board
+      end
 
-      puts "Instructions:"
-      puts "1. Player one always has X and player two always has O."
-      puts "2. Game starts with accepting the player names."
-      puts "3. Game then alteratively allows players to make their moves."
-      puts "4. x axis increments towrds right and y axis increments towards downward direction starting from 0"
-      puts "5. To place X like shown below a player will have to "\
-           "enter 2,0 when his turn comes.\n"\
-           "   To place O like shown below a player will have to"\
-           "enter 1,2 when his turn comes.\n\n"
-      puts introductory_board
+      private
 
-    end
+      def get_position_from_command_line
+        user_position_input = STDIN.gets().strip
+        Position.from_string(user_position_input)
+      rescue Position::IncorrectFormat => e
+        print_error('Please try again with correct format')
+        get_position_from_command_line
+      rescue Position::PositionOutOfBoard => e
+        print_error('Please enter co-ordinates within the board')
+        get_position_from_command_line
+      rescue Position::EmptyString => e
+        print_error('Empty move is invalid')
+        get_position_from_command_line
+      end
 
-    def dashed_line
-      "\n---------------------------------------\n"
+      def dashed_line
+        "\n---------------------------------------\n"
+      end
+
+      def puts_within_dashed_lines(string)
+        puts(dashed_line)
+        puts("\n#{string}\n")
+        puts(dashed_line)
+      end
+
     end
   end
 end
--- spec/command_line_interface_spec.rb
+++ spec/command_line_interface_spec.rb
@@ -6,115 +6,83 @@ module TicTacToe
       $stdout = StringIO.new
     end
 
-    describe 'initialization' do
-      let(:game) { Game.new() }
-      subject { CommandLineInterface.new(game) }
+    after(:each) do
+      $stdout = STDOUT
+    end
 
-      it 'initializes cli object' do
-        expect(subject).to be_an_instance_of(CommandLineInterface)
+    describe "#print_error" do
+      let(:emsg) { 'To err is human' }
+      it "prints error" do
+        CommandLineInterface.print_error(emsg)
+        expect($stdout.string).to include(emsg)
       end
     end
 
-    describe '#start' do
-      # It should act as an interface to execute actions given by
-      # the game until game gives announce_victory or announce_draw command
-
-      let(:game) { Game.new() }
-      let(:announce_draw_action) { double(name: :announce_draw, errors: []) }
-      let(:announce_victory_action) { double(name: :announce_victory, errors: [], details: {name: 'p1'}) }
-      let(:introduction_action) { double(name: :give_introduction, errors: []) }
-      let(:get_p1_name_action) { double(name: :get_p1_name, errors: []) }
-      let(:get_p2_name_action) { double(name: :get_p2_name, errors: []) }
-
-
-      let(:cli) { CommandLineInterface.new(game) }
-
-      it 'Acts as an interface for "announce_draw" game action' do
-        allow(game).to receive(:action_to_perform).and_return(announce_draw_action)
-        cli.start
-        expect($stdout.string).to include("Game was a draw")
-        expect($stdout.string).to include(game.board.to_s)
-      end
-
-      it 'Acts as an interface for "announce_victory" game action' do
-        allow(game).to receive(:action_to_perform).and_return(announce_victory_action)
-        cli.start
-        expect($stdout.string).to include("p1 won the game")
-        expect($stdout.string).to include(game.board.to_s)
-      end
-
-      # Note that game should always return victory or draw game action
-      # at some point otherwise cli will never end
-      it 'Acts as an interface for "give_introduction" game action and starts the game' do
-        allow(game).to receive(:action_to_perform).and_return(introduction_action, announce_draw_action)
-        expect(game).to receive(:start)
-        cli.start
+    describe "#print_introduction" do
+      it "prints introduction to command line" do
+        CommandLineInterface.print_introduction
         expect($stdout.string).to include('Instructions')
       end
+    end
 
-      it 'Acts as an interface for get_p1_name action and triggers accept_p1_name event on game' do
-        allow(game).to receive(:action_to_perform).and_return(get_p1_name_action, announce_draw_action)
-        allow(STDIN).to receive(:gets).and_return('awesome_player1')
-        expect(game).to receive(:accept_p1_name).with('awesome_player1')
-        cli.start
-        expect($stdout.string).to include('Please enter p1 name')
+    describe "#get_name" do
+      subject { CommandLineInterface.get_name("Player 1") }
+      it "asks for and accepts name from command line" do
+        allow(STDIN).to receive(:gets).and_return('Ticker Tacker')
+        expect(subject).to eq('Ticker Tacker')
+        expect($stdout.string).to include('Please enter Player 1 name')
       end
+    end
 
-      it 'Acts as an interface for get_p2_name action and triggers accept_p2_name event on game' do
-        allow(game).to receive(:action_to_perform).and_return(get_p2_name_action, announce_draw_action)
-        allow(STDIN).to receive(:gets).and_return('awesome_player2')
-        expect(game).to receive(:accept_p2_name).with('awesome_player2')
-        cli.start
-        expect($stdout.string).to include('Please enter p2 name')
-      end
+    describe "#get_move" do
+      let(:board) { Board.new() }
+      subject { CommandLineInterface.get_move(board, 'Tick Tack Toer') }
 
-      context 'When game asks for accepting player moves actions' do
-        let(:get_p1_move_action) { double(name: :get_p1_move, errors: []) }
-        let(:get_p2_move_action) { double(name: :get_p2_move, errors: []) }
-
-        context 'When position lies in a tic_tac_toe board' do
-          it 'Acts as an interface for get_p1_move action and triggers accept_p1_move event on game' do
-            allow(game).to receive(:action_to_perform).and_return(get_p1_move_action, announce_draw_action)
-            allow(STDIN).to receive(:gets).and_return('1,2')
-            expect(game).to receive(:accept_p1_move).with(Position.new(x:1, y:2))
-            cli.start
-            expect($stdout.string).to include('Please enter p1 move')
-            expect($stdout.string).to include(game.board.to_s)
-          end
-
-          it 'Acts as an interface for get_p2_move action and triggers accept_p2_move event on game' do
-            allow(game).to receive(:action_to_perform).and_return(get_p2_move_action, announce_draw_action)
-            allow(STDIN).to receive(:gets).and_return('1,2')
-            expect(game).to receive(:accept_p2_move).with(Position.new(x:1, y:2))
-            cli.start
-            expect($stdout.string).to include('Please enter p2 move')
-            expect($stdout.string).to include(game.board.to_s)
-          end
-        end
-
-        context 'When position lies out of tic_tac_toe board or format is incorrect' do
-          it 'Asks for position again and again till valid position in entered' do
-            allow(game).to receive(:action_to_perform).and_return(get_p1_move_action, announce_draw_action)
-            allow(STDIN).to receive(:gets).and_return('','1 2', '1-2', '4,3', '-1, 2', '1,2')
-            expect(game).to receive(:accept_p1_move).with(Position.new(x:1, y:2))
-            cli.start
-            expect($stdout.string).to include('Please enter p1 move')
-            expect($stdout.string).to include('Please enter co-ordinates within the board')
-            expect($stdout.string).to include('Please try again with correct format')
-          end
+      context 'When position lies in a tic_tac_toe board' do
+        it 'It asks for and returns position' do
+          allow(STDIN).to receive(:gets).and_return("1,2")
+          expect(subject).to eq(Position.new(x: 1, y: 2))
+          expect($stdout.string).to include('Tick Tack Toer, please enter your move')
+          expect($stdout.string).to include(board.to_s)
         end
       end
 
-      context 'Action to perform has errors' do
-        let(:emsg1) { 'To err is human' }
-        let(:emsg2) { 'To re err is stupid human' }
-        let(:some_action_with_errors) { double(name: :some_action, errors: [emsg1, emsg2]) }
-        it 'Prints any error if game action to implement has errors' do
-          allow(game).to receive(:action_to_perform).and_return(some_action_with_errors, announce_draw_action)
-          cli.start
-          expect($stdout.string).to include(emsg1)
-          expect($stdout.string).to include(emsg2)
+      context 'When position lies out of tic_tac_toe board or format is incorrect' do
+        it 'Asks for position again and again till valid position in entered' do
+          allow(STDIN).to receive(:gets).and_return('','1 2', '1-2', '4,3', '-1, 2', '1,2')
+          expect(subject).to eq(Position.new(x: 1, y: 2))
+          expect($stdout.string).to include('Tick Tack Toer, please enter your move')
+          expect($stdout.string).to include('Tick Tack Toer, please enter your move')
+          expect($stdout.string).to include('Empty move is invalid')
+          expect($stdout.string).to include('Please try again with correct format')
+          expect($stdout.string).to include('Please enter co-ordinates within the board')
         end
+      end
+    end
+
+    describe "#print_board" do
+      let(:board) { Board.new }
+      it 'Prints the board' do
+        CommandLineInterface.print_board(board)
+        expect($stdout.string).to include(board.to_s)
+      end
+    end
+
+    describe "#announce_draw" do
+      let(:board) { Board.new }
+      it "prints game drawn message" do
+        CommandLineInterface.announce_draw(board)
+        expect($stdout.string).to include('Game was a draw')
+        expect($stdout.string).to include(board.to_s)
+      end
+    end
+
+    describe "#announce_victory" do
+      let(:board) { Board.new }
+      it "prints game drawn message" do
+        CommandLineInterface.announce_victory(board, 'My player')
+        expect($stdout.string).to include('My player won the game')
+        expect($stdout.string).to include(board.to_s)
       end
     end
   end
